b061528b3b70b8bad99c1e8be5a3a2e2eb8d2800
Afribotke <267673311+Afribotke@users.noreply.github.com>
2026-06-13 19:09:08 +0300

    Add files via upload

---
+++ lib/dashboard/menu.ts
@@ -0,0 +1,7 @@
+﻿export const MENU = [
+  { label: "Overview", href: "/dashboard" },
+  { label: "Students", href: "/dashboard/students" },
+  { label: "Teachers", href: "/dashboard/teachers" },
+  { label: "Classes", href: "/dashboard/classes" },
+  { label: "Finance", href: "/dashboard/finance" },
+];
