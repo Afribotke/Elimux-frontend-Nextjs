e5feb6403e5c888ebe2bb3ab68bf4b0abd69e485
Afribotke <267673311+Afribotke@users.noreply.github.com>
2026-06-16 19:26:19 +0300

    fix: add getSupabaseClient export

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -4,3 +4,7 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
+export function getSupabaseClient() {
+  return supabase;
+}
