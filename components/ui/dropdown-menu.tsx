67b6ce252bc20097abf50649a6d3e9eea9a1243e
Afribotke <267673311+Afribotke@users.noreply.github.com>
2026-06-21 19:38:50 +0300

    fix: add badge + dropdown-menu + force install deps

---
+++ components/ui/dropdown-menu.tsx
@@ -0,0 +1,21 @@
+﻿"use client";
+
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
+import { cn } from "@/lib/utils";
+
+export const DropdownMenu = DropdownMenuPrimitive.Root;
+export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
+
+export function DropdownMenuContent({ className, ...props }) {
+  return (
+    <DropdownMenuPrimitive.Content
+      className={cn(
+        "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-white p-1 shadow-md",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export const DropdownMenuItem = DropdownMenuPrimitive.Item;
